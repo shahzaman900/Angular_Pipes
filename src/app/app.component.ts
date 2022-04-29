3b3ff5c1d7443f632858790b2793632fb44c132a
Shah Zaman <shahzamanlive2@gmail.com>
2022-04-29 11:16:00 +0500

    upto custom pipes complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pipes';
+    RecieverName: string = '';
+    paymentDate: string = '';
+    amount = 0;
+    miles: number = 0;
+
+    onMileChange(value: any) {
+        this.miles = parseFloat(value);
+    }
+
+    recieverInput(eventNames: any) {
+        let name = eventNames.target.value;
+        this.RecieverName = name;
+    }
+
+    paymentInput(eventNames: any) {
+        let name = eventNames.target.value;
+        this.paymentDate = name;
+    }
+
+    amountInput(eventNames: any) {
+        let name = eventNames.target.value;
+        this.amount = name;
+    }
 }
